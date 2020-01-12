fac88c135181eed1ac504f395bbc3d883d5bf8fd
Igor <igor.ogerchuk@gmail.com>
2020-01-12 22:53:26 +0200

    Updates

---
+++ precache-manifest.525b9b2141652562f683d147244a1ca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd86dd24ba9b8e425937d7ec6ba317f",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "4e14b149c92b997af021",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.1f1073e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.1f1073e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f938c8115a33eded89c",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.38dd4948.chunk.js"
+  },
+  {
+    "revision": "8ef94a824e6f3ee6d7e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b34c3905.js"
+  }
+]);
